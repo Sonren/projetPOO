a964da36b6dc118be7785810eb33c47c548cfe8a
Hugo PIARD <E229657C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-11-17 12:17:26 +0100

    debut de creation de la classe animal

---
+++ classanimal.java
@@ -0,0 +1,18 @@
+public class  Animal {
+    private String name;
+    private int levellife;
+    private int strength;
+    private Position position;
+    private Biome biome;
+
+    public Animal (String n, int l, int s , Position p, Biome b){
+        this.name = n;
+        this.levellife = l;
+        this.strength = s;
+        this.position = p;
+        this.biome = b;
+    }
+
+    
+}
+
