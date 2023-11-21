cf67a71be7028a2f8bc0766289cebe56cf1ecb84
Hugo PIARD <E229657C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-11-21 14:56:18 +0100

    création des get et des sets

--- classanimal.java
+++ classanimal.java
@@ -12,7 +12,37 @@ public class  Animal {
         this.position = p;
         this.biome = b;
     }
-
-    
+    //fonction get
+    public String getname (){
+        return this.name;
+    }
+     public int getlevellife (){
+        return this.levellife;
+    }
+     public int getstrenght (){
+        return this.strength;
+    }
+      public Position getposition (){
+        return this.position;
+    }
+       public Biome getbiome (){
+        return this.biome;
+    }
+    //fonction set
+    public void setname (String nom){
+        this.name = nom;
+    }
+    public void setlevellife (int level){
+        this.levellife = level;
+    }
+     public void setstrenght (int force){
+        this.strength = force;
+    }
+     public void setposition (Position posi){
+        this.position = posi;
+    }
+      public void setbiome (Biome bio){
+        this.biome = bio;
+    }
 }
 
