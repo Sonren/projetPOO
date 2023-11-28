d5a37b7fb87cf565d297cb37534f04f0dead15ac
Hugo PIARD <E229657C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-11-28 15:47:56 +0100

    changement sur la classe Biome

---
+++ code_source/class_biome/Biome.java
@@ -0,0 +1,61 @@
+package class_biome;
+
+import java.util.ArrayList;
+import class_vegetal.Vegetal;
+import class_Animal.Animal;
+
+public class Biome {
+	private String nom;
+	private ArrayList<Animal> faune;
+	private ArrayList<Vegetal> flore;
+
+	public Biome(String nom){
+		this.nom = nom;
+		this.faune = new ArrayList<>();
+		this.flore = new ArrayList<>();
+	}
+
+	public String getName(){
+		return nom;
+	}
+
+	public void afficheFaune (){
+		System.out.println("Faune dans" + nom + ":");
+		for (Animal animal: faune){
+			System.out.println(animal.getname());
+		}
+	}
+
+	public void afficheFlore (){
+		System.out.println("Flore dans" + nom + ":");
+		for (Vegetal vegetal: flore){
+			System.out.println(vegetal.getname());
+		}
+	}
+
+	public void addAnimaux (Animal animal){
+		faune.add(animal);
+		System.out.println(animal.getname() + "ajouté(e) à la faune de la" + nom);
+	}
+
+	public void addPlante (Vegetal vegetal){
+		flore.add(vegetal);
+		System.out.println(vegetal.getname() + "ajouté(e) à la flore de la" + nom);
+	}
+
+	public void removePlante (Vegetal vegetal){
+		if (flore.remove(vegetal)){
+			System.out.println(vegetal.getname() + "a été extirpé(e) de la flore de la " + nom);
+		}else{
+			System.out.println(vegetal.getname() + "inexistant(e) dans la flore de la" + nom);
+		}
+	}
+
+	public void removeAnimal (Animal animal){
+		if (faune.remove(animal)){
+			System.out.println(animal.getname() + "a été éléminé(e) de la faune de la " + nom);
+		}else{
+			System.out.println(animal.getname() + "inexistant(e) dans la faune de la" + nom);
+		}
+	}
+}
