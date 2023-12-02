513b499987fe0dd678ba8b8211f4916a151a3669
Hugo Piard <hugo.piard@etu.univ-nantes.fr>
2023-12-02 15:56:22 +0100

    modification de la classe Homme

--- code_source/class_Animal/Homme/Homme.java
+++ code_source/class_Animal/Homme/Homme.java
@@ -1,5 +1,24 @@
-package class_Animal;
+package class_Animal.Homme;
+
+import class_Animal.Omnivore;
+import class_Biome.Biome;
+import class_Carte.Position;
 
 public class Homme extends Omnivore {
+    public Inventaire invent;
+
+    public Homme(String n, int l, int s,Position p, Biome b,Inventaire invent) {
+        super(n,l,s,p,b);
+        this.invent = invent;
+    }
     
+    //getters et setters
+    public Inventaire getInvent() {
+        return this.invent;
+    }
+
+    public void setInvent(Inventaire invent) {
+        this.invent = invent;
+    }
+
 }
