54e77104d04d75e7867c3e81d6b730fdf5c4d62e
Hugo PIARD <E229657C@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-12-05 12:34:00 +0100

    changement classe carte

--- code_source/class_Carte/Carte.java
+++ code_source/class_Carte/Carte.java
@@ -8,16 +8,16 @@ import java.util.Random;
 
 public class Carte {
 
-    private ArrayList<Biome> map = new ArrayList<>();
+    private ArrayList<Case> map = new ArrayList<>(); 
     private int nbtour;
     
 
     //getters et setters
-    public ArrayList<Biome> getMap() {
+    public ArrayList<Case> getMap() {
         return this.map;
     }
 
-    public void setMap(ArrayList<Biome> map) {
+    public void setMap(ArrayList<Carte> map) {
         this.map = map;
     }
 
