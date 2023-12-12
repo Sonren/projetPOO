d57f6f2505f25af473c2ac200b87587ca91ea7c1
Hugo PIARD <hugo.piard@etu.univ-nantes.fr>
2023-12-12 14:52:04 +0000

    changement dans la classe case

--- code_source/class_Carte/Case.java
+++ code_source/class_Carte/Case.java
@@ -1,18 +1,18 @@
-package class_Carte;
+package fr.lynchmaniac.class_Carte;
 
 
-import class_Biome.Biome;
+import fr.lynchmaniac.class_Biome.Biome;
 
 public class Case {
 
    private Biome biome;
    private Position position;
+   private boolean isvegetal = false;
+   private boolean isanimal = false;
+   private boolean isrocher = false;
+   private boolean ischampi = false;
     
-
-
-    public Case(Biome biome, Position position) {
-        this.biome = biome;
-        this.position = position;
+    public Case() {
     }
 
     //getters et setters
@@ -31,6 +31,40 @@ public class Case {
     public void setPosition(Position position) {
         this.position = position;
     }
+
+    public void setisvegetal(boolean b){
+        this.isvegetal = b;
+    }
+
+    public boolean getisvegetal(){
+        return this.isvegetal;
+    }
+    
+    public void setisanimal(boolean b){
+        this.isanimal = b;
+    }
+
+    public boolean getisanimal(){
+        return this.isanimal;
+    }
+
+    public boolean getisrocher(){
+        return this.isrocher;
+    }
+
+    public void setisrocher(Boolean r){
+        this.isrocher = r;
+    }
+
+    public void setischampi(boolean ch){
+        this.ischampi = ch;
+    }
+
+    public boolean setischampi(){
+        return this.ischampi;
+    }
+
+    
 
     
 }
