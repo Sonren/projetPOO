c4d6fb1056fd553a7e08abfba7b0659f82089cf8
AGBOKOU Eric-Henry <eric.agbokou@etu.univ-nantes.fr>
2023-12-14 21:43:35 +0100

    Ajout d'un fichier testVegetal pour tester les méthodes de la classe Vegetal + Javadoc class_Vegetal

--- code_source/class_Vegetal/Arbre.java
+++ code_source/class_Vegetal/Arbre.java
@@ -3,18 +3,39 @@ package class_Vegetal;
 import class_Biome.Biome;
 import class_Carte.Position;
 
+/**
+ * La classe Arbre représente un type spécifique de végétal, un arbre.
+ */
 public class Arbre extends Vegetal{
+
+    /** La hauteur de l'arbre. */
     private int hauteur = 0;
 
+    /**
+     * Constructeur de la classe Arbre.
+     *
+     * @param n Le nom de l'arbre.
+     * @param b Le biome dans lequel l'arbre se trouve.
+     * @param p La position de l'arbre sur la carte.
+     */
     public Arbre (String n, Biome b, Position p){
         super(n,b,p);
     }
 
-    //creation getters
+    /**
+     * Obtient la hauteur de l'arbre.
+     *
+     * @return La hauteur de l'arbre.
+     */
     public int gethauteur (){
         return this.hauteur;
     }
-    //creation setters 
+
+    /**
+     * Définit la hauteur de l'arbre.
+     *
+     * @param h La nouvelle hauteur de l'arbre.
+     */
     public void setnbbois (int h){
         this.hauteur = h;
     }
--- code_source/class_Vegetal/Vegetal.java
+++ code_source/class_Vegetal/Vegetal.java
@@ -1,64 +1,129 @@
 package class_Vegetal;
+
 import class_Biome.Biome;
 import class_Carte.Position;
 
+/**
+ * La classe Vegetal représente un élément végétal générique.
+ */
 public class Vegetal {
 
+    /** Le nom du végétal. */
     private String name;
+
+    /** Le biome dans lequel le végétal se trouve. */
     private Biome biome;
+
+    /** La position du végétal sur la carte. */
     private Position position;
+
+    /** Indique si le végétal est mort ou non. */
     private boolean isDeadveg = false;
+
+    /** Le nombre de bois associé au végétal. */
     private int nbbois = 2;
 
-    //constructeur
+    /**
+     * Constructeur de la classe Vegetal.
+     *
+     * @param n Le nom du végétal.
+     * @param b Le biome dans lequel le végétal se trouve.
+     * @param p La position du végétal sur la carte.
+     */
     public  Vegetal (String n, Biome b, Position p){
             this.name = n;
             this.biome = b;
             this.position = p;
     }
 
-    //les getter
+    /**
+     * Obtient le nom du végétal.
+     *
+     * @return Le nom du végétal.
+     */
     public String getname(){
         return this.name;
     }
     
+    /**
+     * Obtient le biome dans lequel le végétal se trouve.
+     *
+     * @return Le biome du végétal.
+     */
     public Biome getbiome(){
         return this.biome;
     }
 
+    /**
+     * Obtient la position du végétal sur la carte.
+     *
+     * @return La position du végétal.
+     */
     public Position getposition(){
         return this.position;
     }
 
+    /**
+     * Vérifie si le végétal est mort.
+     *
+     * @return True si le végétal est mort, sinon False.
+     */
     public boolean isDeadveg(){
         return this.isDeadveg;
     }
 
+    /**
+     * Obtient le nombre de bois associé au végétal.
+     *
+     * @return Le nombre de bois associé au végétal.
+     */
     public int getnbbois(){
         return this.nbbois;
     }
 
-    // les setters
+    /**
+     * Définit le nom du végétal.
+     *
+     * @param nom Le nouveau nom du végétal.
+     */
     public void setname (String nom){
         this.name = nom;
     }
 
+    /**
+     * Définit le biome dans lequel le végétal se trouve.
+     *
+     * @param bio Le nouveau biome du végétal.
+     */
     public void setbiome (Biome bio){
         this.biome = bio;
     }
 
+    /**
+     * Définit la position du végétal sur la carte.
+     *
+     * @param emplacement La nouvelle position du végétal.
+     */
     public void setposition (Position emplacement){
         this.position = emplacement;
     }
 
+    /**
+     * Définit si le végétal est mort ou non.
+     *
+     * @param deadv Indique si le végétal est mort.
+     */
     public void setisDeadveg(boolean deadv){
         this.isDeadveg = deadv;
     }
 
+    /**
+     * Définit le nombre de bois associé au végétal.
+     *
+     * @param nb Le nouveau nombre de bois associé au végétal.
+     */
     public void setnbbois(int nb){
         this.nbbois = nb;
     }
-
-
 }
 
